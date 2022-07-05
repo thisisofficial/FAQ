ce5a8bccb9d80a26c4221994ac50287f92ed5588
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-07-04 21:00:20 -0500

    Made fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { FAQ } from './Screens/FAQ.jsx';
+import { FaqScreen } from './Screens/FAQ.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <FAQ></FAQ>
+    <FaqScreen/>
   )
 }
 
--- src/Screens/FAQ.jsx
+++ src/Screens/FAQ.jsx
@@ -6,12 +6,35 @@ import box from '/src/images/bg-pattern-desktop.svg';
 import boxmobile from '/src/images/bg-pattern-mobile.svg';
 import { Question } from './Molecules/Question';
 
-export function FAQ() {
-    const [opened, setOpened] = useState(0);
+export function FaqScreen() {
+    const [opened, setOpened] = useState(null);
+
+    const info =[
+        {
+            question: 'How many team members can I invite?',
+            answer: "Like 2, idk. You probably dont have enough friends to fill a whole room still. Lol.",
+        },
+        {
+            question: 'What is the maximum file upload size?',
+            answer: "No more than 2GB. All files in your account must fit your allotted storage space.",
+        },
+        {
+            question: 'How do I reset my password?',
+            answer: "Contact support at 1-800-eatmyassyoucant or visit our suppor page at: www.myass.eatit/youcant",
+        },
+        {
+            question: 'Can I cancel my subscription?',
+            answer: "Uh...no, why would you anyway? How do you think we make money?. You our bitch now, so open wide.",
+        },
+        {
+            question: 'Do you provide additional support?',
+            answer: "Yes! You may contact our support staff by sending an email to support@meetpage.com. We will reply ASAP.",
+        }
+    ]
 
     const toogleOpened = index => {
         if(opened == index){
-            setOpened(0);
+            setOpened(null);
         }
         else{
             setOpened(index);
@@ -26,40 +49,14 @@ export function FAQ() {
             </ContLeft>
             <ContRight>
                 <Title>FAQ</Title>
-                <Question 
-                toogle={toogleOpened} 
-                index={1}
-                opened={opened}
-                quest="How many team members can I invite?"
-                ans="Like 2, idk. You probably dont have enough friends to fill a whole room still. Lol."/>
-
-                <Question 
-                toogle={toogleOpened} 
-                index={2}
-                opened={opened}
-                quest="What is the maximum file upload size?"
-                ans="No more than 2GB. All files in your account must fit your allotted storage space."/>
-
-                <Question 
-                toogle={toogleOpened} 
-                index={3}
-                opened={opened}
-                quest="How do I reset my password?"
-                ans="Contact support at 1-800-eatmyassyoucant or visit our suppor page at: www.myass.eatit/youcant"/>
-                
-                <Question 
-                toogle={toogleOpened} 
-                index={4}
-                opened={opened}
-                quest="Can I cancel my subscription?"
-                ans="Uh...no, why would you anyway? How do you think we make money?. You our bitch now, so open wide."/>
-                
-                <Question 
-                toogle={toogleOpened} 
-                index={5}
-                opened={opened}
-                quest="Do you provide additional support?"
-                ans="Yes! You may contact our support staff by sending an email to support@meetpage.com. We will reply ASAP."/>
+                {info.map((info, i) => (
+                                    <Question 
+                                    toogle={toogleOpened} 
+                                    index={i}
+                                    opened={opened}
+                                    quest={info.question}
+                                    ans={info.answer}/>
+                ))}
             </ContRight>
         </Cont>
     </App>
